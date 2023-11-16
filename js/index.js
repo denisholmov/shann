319f291440342dddc6847cad49840911b82ce640
Denis <denisskholmov@gmail.com>
2023-11-16 18:29:37 +0300

    Закончил основную часть проекта

--- js/index.js
+++ js/index.js
@@ -4,13 +4,13 @@ const loaderBtn3 = document.querySelector('.loader__btn_3');
 const loaderBack = document.getElementById('loader-back-id');
 const loaderTransparent = document.getElementById('loader-transparent-id');
 const loaderInf = document.getElementById('loader-inf-id');
-const evolutionTech = document.querySelector('.evolution-tech');
+
 
 const loaderBackClass = 'loader__back_none';
 const loaderTransparentClass = 'loader__transparent_none';
 const loaderInfClass = 'loader__inf';
 const loaderInfClassNone = 'loader__inf_none';
-const evolutionTechSliderBlock = 'evolution-tech_sliderBlock';
+
 
 loaderInf.classList.add(loaderInfClassNone);
 
